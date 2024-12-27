3a51ea070f3d1a65399e75b84e00a8b0e4df3708
surya <suryakirank153@gmail.com>
2024-12-27 09:45:26 +0530

    third

--- fun.js
+++ fun.js
@@ -1,5 +1,31 @@
 import React from "react"
-import ReactDom from "react-dom"
+import ReactDom from "react-dom/client"
 
 
-console.log("hi")
+const Header =() => 
+(
+    <div className="header">
+        <div id="logo">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKKdlNSyO831VQzABs2_7phbKaUXziEiWXzg&s"></img>
+        </div>
+        <div id ="info">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>)
+const App = () =>
+    (
+        <div>
+            <Header />
+            
+        </div>
+        
+    )
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+
+root.render(<App />)
